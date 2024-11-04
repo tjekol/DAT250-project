8538a4dd0a73f22f03aa231cabf28870c2230993
Thea Jenny E Kolnes <thea.jenny02@gmail.com>
2024-11-04 12:52:27 +0100

    Users fetched and only stored in db

--- backend/src/main/java/no/hvl/rest/components/UserRepository.java
+++ backend/src/main/java/no/hvl/rest/components/UserRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface UserRepository extends CrudRepository<User, Long> {
-    List<User> findByUsername(String username);
+    User findByUsername(String username);
 
     User findById(long id);
 }
--- backend/src/main/java/no/hvl/rest/controllers/UserController.java
+++ backend/src/main/java/no/hvl/rest/controllers/UserController.java
@@ -23,7 +23,7 @@ public class UserController {
     };
 
     @GetMapping("/users")
-    public ResponseEntity<Set<User>> getUsers() {
+    public ResponseEntity<Iterable<User>> getUsers() {
         return ResponseEntity.ok(manager.getUsers());
     }
 
