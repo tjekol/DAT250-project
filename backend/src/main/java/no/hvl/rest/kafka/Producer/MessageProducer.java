c60d7200d30db2ae6848b7948e7e7e3ef003930b
SindreEieLedsaak <sindre.eie@gmail.com>
2024-11-30 15:12:18 +0100

    Made Kafka messageing async

--- backend/src/main/java/no/hvl/rest/kafka/Producer/MessageProducer.java
+++ backend/src/main/java/no/hvl/rest/kafka/Producer/MessageProducer.java
@@ -2,14 +2,18 @@ package no.hvl.rest.kafka.Producer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Component;
 
 @Component
+@EnableAsync
 public class MessageProducer {
 
     @Autowired
     private KafkaTemplate<String, Object> kafkaTemplate;
 
+    @Async
     public void sendMessage(String topic, Object message) {
         kafkaTemplate.send(topic, message);
         System.out.println("Message: " +message);
